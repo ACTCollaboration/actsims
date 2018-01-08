ba8312eeae9abfc3ca98bd79f5d72a4c940d5ee5
Alexander van Engelen <engelen@cita.utoronto.ca>
2018-01-08 11:49:58 -0800

    updates

--- py/simTools.py
+++ py/simTools.py
@@ -1,46 +1,68 @@
-import numpy, cmblens.flipper.flipperDict as flipperDict, pickle, \
-    cmblens.flipper.liteMap as liteMap
+import numpy as np, cmblens.flipper.flipperDict as flipperDict, pickle, \
+    cmblens.flipper.liteMap as liteMap 
 
 
-def makeActpolNoiseSim(iterationNum = 0, region = 'd56', \
-                       season = 's14', \
-                       pa = 'pa1', \
-                       freqGHz = 150, \
-                       patch = None,\
-                       inDictFile = 'Coadd_s131415.dict', \
-                       dictFilePath = '/global/homes/e/engelen/cmblens/inputParams/', \
-                       noisePsdDir = '/global/homes/e/engelen/cmblens/maps/dataMaps/actpolDeep/', \
-                       verbose = True):
-#FIXME get rid of absolute pathnames
-    """ Return a given noise sim  """
+import warnings
+with warnings.catch_warnings():
+    warnings.filterwarnings("ignore",category=DeprecationWarning)
+import liteMapPol
 
-    p = flipperDict.flipperDict()
-    p.read_from_file(dictFilePath + inDictFile)
+from enlib import enmap
+import pdb
 
-    if patch == None:
+def getActpolCmbSim(beamfile, coords, iterationNum, cmbDir, cmbSet = 0, \
+                    coordsEpsilonArcmin = np.array([[0,0], [0,0]]), \
+                    doBeam = True):
+    """ coords is in radians as np.array( [ [dec0,ra0], [dec1, ra1 ] ])"""
+    #wcs fix per https://phy-wiki.princeton.edu/polwiki/pmwiki.php?n=ACTpolLensingAnalysisTelecon.SomePreliminarySignalSims.  Coords passed in in degrees
 
-        freqStr = (freqGHz if type(freqGHz) is str else 'f%03i' % freqGHz)
-        patch = '%s-%s-%s-%s' %(season, region, pa, freqStr)
+    shape,wcs = enmap.fullsky_geometry(res=1.0*np.pi/180./60.)
 
+    output = [None] * 3
 
-    #unroll patch names (normally stored as a nested  array of arrays)
-    patchList = [item for sublist in p['coaddPatchList'] for item in sublist]
+    # coordsForEnmap = np.array([[coords[1,0], coords[0,1]],[ coords[0,0],coords[1,1]]]) \
+    #                  * np.pi / 180.
 
+    if coords[1,1] > 180. :
+        coords[1,1] -= 360.
+    if coords[0,1] > 180. :
+        coords[0,1] -= 360.
+
+        
+
+    coordsForEnmap = (coords + coordsEpsilonArcmin / 60.) \
+                     * np.pi / 180.
+
+    # pdb.set_trace()
+
+    for x in range(0, 3):
+        thisMap = enmap.read_fits(
+            cmbDir + \
+            "/cmb_set%02d_%05i/fullskyLensedMap_%s_%05d.fits" \
+            % (cmbSet, iterationNum, 'TQU'[x], iterationNum), \
+            box = coordsForEnmap, \
+            wcs_override = wcs )
+
+        # thisMap = enmap.upgrade(thisMap, 2.)
+
+        output[x] = thisMap.to_flipper()
+
+    if doBeam:
+        liteMapPol.simpleBeamConvolution(output[0], output[1], output[2], beamfile )
+
+    return output
     
-    if patch not in patchList:
-        raise ValueError('patch %s not found in patchList; options are ' % (patch ), patchList)
-    mySeed = patchList.index(patch) * 1000000 + iterationNum 
 
+
+def getActpolNoiseSim(noiseSeed, patch, noisePsdDir, mask, verbose=True):
     tNoise = pickle.load(open(noisePsdDir+'noisePowerIAlt_'+patch+'.pkl'))
     qNoise = pickle.load(open(noisePsdDir+'noisePowerQAlt_'+patch+'.pkl'))
     uNoise = pickle.load(open(noisePsdDir+'noisePowerUAlt_'+patch+'.pkl'))
 
-    #FIXME funny naming for now.  Also assumes T weighting for all
-    mask = liteMap.liteMapFromFits(noisePsdDir + 'weightMap_T' + patch + '.fits')
 
-    loc = numpy.where(mask.data == 0.)
-    loc2 = numpy.where(mask.data < 0.)
-    mask.data = numpy.sqrt(mask.data)
+    loc = np.where(mask.data == 0.)
+    loc2 = np.where(mask.data < 0.)
+    mask.data = np.sqrt(mask.data)
 
     TF = mask.copy()
     QF = mask.copy()
@@ -50,8 +72,8 @@ def makeActpolNoiseSim(iterationNum = 0, region = 'd56', \
     UF.data *= 0.
 
     if verbose:
-        print 'makeActpolNoiseSim: setting unique RNG seed of %i' % mySeed
-    numpy.random.seed(mySeed)
+        print 'getActpolNoiseSim: setting unique RNG seed of %i' % noiseSeed
+    np.random.seed(noiseSeed)
     TF.fillWithGRFFromTemplate(tNoise,bufferFactor=1)
     QF.fillWithGRFFromTemplate(qNoise,bufferFactor=1)
     UF.fillWithGRFFromTemplate(uNoise,bufferFactor=1)
@@ -66,13 +88,100 @@ def makeActpolNoiseSim(iterationNum = 0, region = 'd56', \
     TF.data[loc2] = 0.
     QF.data[loc2] = 0.
     UF.data[loc2] = 0.
+    output = [TF, QF, UF]
 
     # TF.writeFits(noiseDir+'/noise_2dPSD_v2_T_%s.fits'%(patch), overWrite=True)
     # QF.writeFits(noiseDir+'/noise_2dPSD_v2_Q_%s.fits'%(patch), overWrite=True)
     # UF.writeFits(noiseDir+'/noise_2dPSD_v2_U_%s.fits'%(patch), overWrite=True)
 
+    return output
+
+
+def getActpolSim(iterationNum = 0, region = 'd5', 
+                 season = 's13', \
+                 pa = 'pa1', \
+                 freqGHz = 150, \
+                 patch = None,\
+                 coaddDictFile = 'Coadd_s131415.dict', \
+                 coaddDictFilePath = '/global/homes/e/engelen/cmblens/inputParams/', \
+                 simToolsDictFile = 'simTools.dict',\
+                 simToolsDictFilePath = '/global/homes/e/engelen/lensSims/inputParams/',\
+                 verbose = True,\
+                 simType = 'noise',
+                 cmbSet = 0,
+                 doBeam = True):
+
+#FIXME: get rid of absolute pathnames
+    """ Return a given noise sim.  
+    Provide either 'patch' or all of 'season', 'freqGHz', 'pa', and 'region' .
+    For now returns only a liteMap. """
+
+    cDict = flipperDict.flipperDict()
+    cDict.read_from_file(coaddDictFilePath + '/' + coaddDictFile)
+
+    sDict = flipperDict.flipperDict()
+    sDict.read_from_file(simToolsDictFilePath + '/' + simToolsDictFile)
+    
+
+
+    if patch == None:
+        freqStr = (freqGHz if type(freqGHz) is str else 'f%03i' % freqGHz)
+        patch = '%s-%s-%s-%s' %(season, region, pa, freqStr)
+
+
+    #unroll patch names (normally stored as a nested  array of arrays)
+    patchList = [item for sublist in cDict['coaddPatchList'] for item in sublist]
+
+
+    if patch not in patchList:
+        raise ValueError('patch %s not found in patchList; options are ' % (patch ), patchList)
+    noiseSeed = patchList.index(patch) * 1000000 + iterationNum 
+
+    foregroundSeed = patchList.index(patch) * 100000000 + iterationNum 
+
+    mask = liteMap.liteMapFromFits(sDict['noisePsdDir'] + 'weightMap_T' + patch + '.fits')
+    if verbose:
+        print 'coords are (x0, y0, x1, y1) = (', mask.x0, mask.y0, mask.x1, mask.y1, ')'
+
+    if simType == 'noise':
+
+# def makeActpolNoiseSim(noiseSeed, patch, noisePsdDir, mask)
+
+        return getActpolNoiseSim(noiseSeed = noiseSeed, \
+                                  patch = patch, \
+                                  noisePsdDir = sDict['noisePsdDir'],
+                                  mask = mask,
+                                  verbose = verbose)
+
+    elif simType == 'cmb':
+# def getActpolCMBSim(beamfile, coords, iterationNum, cmbDir):
+        
+        #note dec comes first in the array ordering for enmap, so follow that here
+        return getActpolCmbSim(beamfile = sDict['beamNames'][patch],
+                               coords = np.array([[mask.y0, mask.x0],[mask.y1, mask.x1]] ),
+                               iterationNum = iterationNum,
+                               cmbDir = sDict['cmbDir'], cmbSet = 0, \
+                               coordsEpsilonArcmin = np.array(sDict['coordsEpsilonArcminCMBSim']),\
+                               doBeam = doBeam)
+
+    elif simType == 'foregrounds':
+        raise ValueError("not yet implemented")
+
+    else:
+        raise ValueError("bad input")
+    
+# def getActpolCmbSim(iterationNum = 0, region = 'd56', \
+#                      season = 's14', \
+#                      pa = 'pa1', \
+#                      freqGHz = 150, \
+#                      patch = None,\
+#                      coaddDictFile = 'Coadd_s131415.dict', \
+#                      dictFilePath = '/global/homes/e/engelen/cmblens/inputParams/', \
+#                      noisePsdDir = '/global/homes/e/engelen/cmblens/maps/dataMaps/actpolDeep/', \
+#                      verbose = True)
 
 
 
 
-    return [TF, QF, UF]
+
+
