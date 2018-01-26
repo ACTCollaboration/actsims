838a1861bfe4fa2e2901d98957c8525dd5898261
Alexander van Engelen <engelen@cita.utoronto.ca>
2018-01-25 17:48:47 -0800

    tweaks

--- bin/simpleTest.py
+++ bin/simpleTest.py
@@ -3,9 +3,24 @@ from actsims import simTools
 
 
 #patch can be currently any of:
+#                   [['deep1_s13_pa1_f150'] ,\
+#                    ['deep5_s13_pa1_f150'],\
+#                    ['deep6_s13_pa1_f150'],\
+                   
+#                    ['deep56_s14_pa1_f150'],\
+#                    ['deep56_s14_pa2_f150'],\
+#                    ['deep56_s15_pa1_f150'],\
+#                    ['deep56_s15_pa2_f150'],\
+#                    ['deep56_s15_pa3_f090'],\
+#                    ['deep56_s15_pa3_f150'],\
+#                    ['deep8_s15_pa1_f150'],\
+#                    ['deep8_s15_pa2_f150'],\
+#                    ['deep8_s15_pa3_f090'],\
+#                    ['deep8_s15_pa3_f150']]
 
 
 
+#Alternatively, you can pass the info separately, e.g: season = 's13', pa = 'pa1', freqGHz = 150, region = 'deep5'
 
 patch = 'deep8_s15_pa1_f150'
 
