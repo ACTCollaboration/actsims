147c300ee7e3f5d57450e7c5672db5749812f4aa
dwhan89 <retimes@hotmail.com>
2019-03-08 12:06:14 -0800

    fg only sim call

--- actsims/signal.py
+++ actsims/signal.py
@@ -1,7 +1,8 @@
 import numpy as np, os
 from pixell import enmap, powspec, curvedsky, fft as pfft
 from orphics import io
-from . import simTools, util, act_data_model
+from . import simTools, util
+from soapack import interfaces as sints
 from soapack.interfaces import ACTmr3 
 import healpy as hp
 import warnings
@@ -9,26 +10,35 @@ from collections import OrderedDict as ODict
 
 actsim_root = os.path.dirname(os.path.realpath(__file__))
 
-try: paths = io.config_from_yaml(os.path.join(actsim_root, "../inputParams/paths.yml"))
-except:
-    paths = io.config_from_yaml(os.path.join(actsim_root, "../inputParams/paths_example.yml"))
-    warnings.warn("No input/paths.yml found. Using version controlled input/paths_example.yml. Please copy and edit your local version.")
+class SignalModel(object):
+    # a helper class to quickly generate sims for given patch
+    def __init__(self, cmb_type='LensedCMB', dobeam=True, add_foregrounds=True, apply_window=True, max_cached=1, data_model=None):
+        self.data_model = ACTmr3() if data_model is None else data_model
+        self.cmb_types   = ['LensedCMB', 'UnlensedCMB', 'LensedUnabberatedCMB']
+        paths            = sints.dconfig['actsims']
+        self.signal_path = paths['signal_path']
+        assert(self.signal_path is not None)
+        assert(cmb_type in self.cmb_types)
 
-alm_root  = paths['alm_root']
-map_root  = paths['map_root']
-pout      = paths['plots'] 
-mask_root = paths['mask_root']
 
-class DataModelMock(ACTmr3):
-    # will swap with the final version of data model 
-    def __init__(self):
         self.supported_sims = ['s13_pa1_deep1_f150', 's13_pa1_deep5_f150', 's13_pa1_deep6_f150', 's14_pa1_deep56_f150', 's14_pa2_deep56_f150', \
                 's15_pa1_boss_f150', 's15_pa1_deep56_f150', 's15_pa1_deep8_f150', 's15_pa2_boss_f150', 's15_pa2_deep56_f150', 's15_pa2_deep8_f150',\
                 's15_pa3_boss_f090', 's15_pa3_boss_f150', 's15_pa3_deep56_f090', 's15_pa3_deep56_f150', 's15_pa3_deep8_f090', 's15_pa3_deep8_f150']
         self.supported_sims.sort()
 
-        self.cmb_types      = ['LensedCMB', 'UnlensedCMB', 'LensedUnabberatedCMB']
         self.freqs          = ['f090', 'f150']
+        self.cmb_type        = cmb_type
+        self.max_cached      = max_cached
+        self.alms_base       = ODict()
+        self.alms_cmb        = ODict()
+        self.alms_fg         = ODict()
+        self.alms_patch      = ODict()
+        self.signals         = ODict()
+        self.templates       = ODict()
+        self.apply_window    = apply_window
+        self.add_foregrounds = add_foregrounds
+        self.dobeam          = dobeam
+
     def is_supported(self, sesaon, array, patch, freq):
         signal_idx      = self.__combine_idxes__(sesaon, array, patch, freq)
         supported = signal_idx in self.supported_sims
@@ -40,23 +50,7 @@ class DataModelMock(ACTmr3):
 
     def __combine_idxes__(self, season, array, patch, freq):
         return '_'.join([season, array, patch, freq])
-
-class SignalModel(DataModelMock):
-    # a helper class to quickly generate sims for given patch
-    def __init__(self, cmb_type='LensedCMB', dobeam=True, add_foregrounds=True, apply_window=True, max_cached=1):
-        DataModelMock.__init__(self)
-        assert(cmb_type in self.cmb_types)
-
-        self.cmb_type        = cmb_type
-        self.max_cached      = max_cached
-        self.alms_base       = ODict()
-        self.alms_patch      = ODict()
-        self.signals         = ODict()
-        self.templates       = ODict()
-        self.apply_window    = apply_window
-        self.add_foregrounds = add_foregrounds
-        self.dobeam          = dobeam
-
+    
     def get_base_alm_idx(self, set_idx, sim_num):
         return '_'.join(['set0%d'%set_idx, '%05d'%sim_num])
 
@@ -86,7 +80,6 @@ class SignalModel(DataModelMock):
         alm_patch = None
         if self.alms_patch.has_key(signal_idx):  
             print ("loading precomputed alm {}".format(signal_idx))
-            return self.signals[signal_idx].copy()
             alm_patch = self.alms_patch[signal_idx].copy()
         else:
             freq_idx  = 0 if freq == 'f090' else 1
@@ -96,14 +89,75 @@ class SignalModel(DataModelMock):
             alm_patch = self.alms_base[base_alm_idx][freq_idx].copy()
             if self.dobeam:
                 print ("apply beam for alm {}".format(signal_idx))
-                _, beam_data = act_data_model.load_normalized_beam(season, array, patch, freq)
-                for idx in range(alm_patch.shape[0]):
-                    alm_patch[idx] = hp.sphtfunc.almxfl(alm_patch[idx].copy(), beam_data)
+                alm_patch = self.__apply_beam__(alm_patch, season, array, patch, freq)
             else: pass
             if save_alm: 
                 self.manage_cache(self.alms_patch, self.max_cached-1) 
                 self.alms_patch[signal_idx] = alm_patch.copy()
+        
+        return self.__signal_postproessing__(patch, signal_idx, alm_patch, save_map=save_map)
+ 
+    def get_cmb_sim(self, season, array, patch, freq, set_idx, sim_num, save_alm=False):
+        assert(self.is_supported(season, array, patch, freq))
+        print("[WARNING] get_cmb_sim() is implemented for debugging purpose. Use get_signal_sim() for the production run")
 
+        signal_idx   = self.get_signal_idx(season, array, patch, freq, set_idx, sim_num)
+        
+        alm_cmb = None
+        if self.alms_cmb.has_key(signal_idx):  
+            print ("loading precomputed alm cmb {}".format(signal_idx))
+            alm_cmb = self.alms_cmb[signal_idx].copy()
+        else:
+            alm_cmb = self.load_alms_base(set_idx, sim_num, cache=False, fg_override=False, ret_alm=True)
+            freq_idx  = 0 if freq == 'f090' else 1
+            alm_cmb = alm_cmb[freq_idx].copy()
+            if self.dobeam:
+                print ("apply beam for alm {}".format(signal_idx))
+                alm_cmb = self.__apply_beam__(alm_cmb, season, array, patch, freq)
+            else: pass
+            if save_alm: 
+                self.manage_cache(self.alms_cmb, self.max_cached-1) 
+                self.alms_cmb[signal_idx] = alm_cmb.copy()
+
+        return self.__signal_postproessing__(patch, signal_idx, alm_cmb, save_map=False)
+
+    def get_fg_sim(self, season, array, patch, freq, set_idx, sim_num, save_alm=False):
+        assert(self.is_supported(season, array, patch, freq))
+        print("[WARNING] get_fg_sim() is implemented for debugging purpose. Use get_signal_sim() for the production run")
+
+        signal_idx   = self.get_signal_idx(season, array, patch, freq, set_idx, sim_num)
+        alm_fg = None
+        if self.alms_fg.has_key(signal_idx):  
+            print ("loading precomputed alm cmb {}".format(signal_idx))
+            alm_fg = self.alms_fg[signal_idx].copy()
+        else:
+            alm_fg90_150  = self.load_alm_fg(set_idx, sim_num) 
+            alm_out = np.zeros((3, len(alm_fg90_150[-1])), dtype = np.complex128) 
+
+            freq_idx      = 1 if freq == 'f150' else 0
+            alm_out[0, :] = alm_fg90_150[freq_idx, :].copy()
+       
+            alm_fg = alm_out
+            if self.dobeam:
+                print ("apply beam for alm {}".format(signal_idx))
+                alm_fg = self.__apply_beam__(alm_fg, season, array, patch, freq)
+            else: pass
+            if save_alm: 
+                self.manage_cache(self.alms_fg, self.max_cached-1) 
+                self.alms_fg[signal_idx] = alm_fg.copy()
+
+        return self.__signal_postproessing__(patch, signal_idx, alm_fg, save_map=False)
+    
+    def __apply_beam__(self, alm_patch, season, array, patch, freq):
+        lmax      = hp.Alm.getlmax(alm_patch.shape[-1])
+        l_beam    = np.arange(0, lmax+100, dtype=np.float)
+        beam_data = self.data_model.get_beam(l_beam, season, patch, '{}_{}'.format(array, freq)) 
+        
+        for idx in range(alm_patch.shape[0]):
+            alm_patch[idx] = hp.sphtfunc.almxfl(alm_patch[idx].copy(), beam_data)
+        return alm_patch 
+
+    def __signal_postproessing__(self, patch, signal_idx, alm_patch, save_map):
         signal = self.get_template(patch)
         curvedsky.alm2map(alm_patch, signal, spin = [0,2], verbose=True)
         
@@ -123,32 +177,37 @@ class SignalModel(DataModelMock):
             self.manage_cache(self.signals, self.max_cached-1)
             self.signals[signal_idx] = signal.copy()
         return signal
- 
 
     def get_template(self, patch):
         if not self.templates.has_key(patch):
             self.manage_cache(self.templates, self.max_cached-1) 
-            template_file = os.path.join(mask_root, '{}_mr3c_20181012_190203_master_apo_w0.fits'.format(patch))
-            template      = enmap.read_map(template_file)
+            template      = self.data_model.get_mask(patch)
             self.templates[patch] = enmap.empty((3,) + template.shape, template.wcs)
         else: pass
         return self.templates[patch].copy()
 
-    def load_alms_base(self, set_idx, sim_idx):
+    def load_alm_fg(self, set_idx, sim_idx):
+        print("loading fg alm")
+        seed         = (set_idx, 0, 1, sim_idx)# copying the structure from simtools
+        fg_file      = os.path.join(actsim_root, '../data/fg.dat')
+        fg_power     = powspec.read_spectrum(fg_file, ncol = 3, expand = 'row')
+        alm_fg90_150 = curvedsky.rand_alm(fg_power, seed = seed)#, lmax=lmax_sg)
+        return alm_fg90_150
+
+    def load_alms_base(self, set_idx, sim_idx, cache=True, fg_override=None, ret_alm=False):
         # note: beam is set to false
         print("loading alm base")
-        cmb_file   = os.path.join(alm_root, 'fullsky%s_alm_set%02d_%05d.fits' %(self.cmb_type, set_idx, sim_idx))
+        cmb_file   = os.path.join(self.signal_path, 'fullsky%s_alm_set%02d_%05d.fits' %(self.cmb_type, set_idx, sim_idx))
         print("loading %s" %cmb_file)
         alm_signal = np.complex128(hp.fitsfunc.read_alm(cmb_file, hdu = (1,2,3))) 
         alm_signal = np.tile(alm_signal, (len(self.freqs), 1, 1))
-        if self.add_foregrounds:
-            print("adding fgs to the base")
-            seed         = (set_idx, 0, 1, sim_idx)# copying the structure from simtools
-            fg_file      = os.path.join(actsim_root, '../data/fg.dat')
-            fg_power     = powspec.read_spectrum(fg_file, ncol = 3, expand = 'row')
-            
+
+        if fg_override is not None: print("[Warning] override the default foreground flag....")
+        add_foregrounds = fg_override if fg_override is not None else self.add_foregrounds
+        if add_foregrounds:
+            print("adding fgs to the base")     
+            alm_fg90_150 = self.load_alm_fg(set_idx, sim_idx)
             lmax_sg      = hp.Alm.getlmax(alm_signal.shape[-1])
-            alm_fg90_150 = curvedsky.rand_alm(fg_power, seed = seed)#, lmax=lmax_sg)
             alm_out      = np.zeros((len(self.freqs), 3, len(alm_fg90_150[-1])), dtype = np.complex128) 
             lmax_fg      = hp.Alm.getlmax(alm_fg90_150.shape[-1])
 
@@ -170,7 +229,9 @@ class SignalModel(DataModelMock):
             alm_signal = alm_out.copy()
             del alm_out, alm_fg90_150
 
-        self.alms_base[self.get_base_alm_idx(set_idx, sim_idx)] = alm_signal.copy()
+        if cache: self.alms_base[self.get_base_alm_idx(set_idx, sim_idx)] = alm_signal.copy()
+        
+        if ret_alm: return alm_signal
         del alm_signal
 
 
